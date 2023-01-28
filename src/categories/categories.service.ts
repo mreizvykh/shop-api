8979340f761dbcb01ea1417530ca8e8d7029609f
mreizvykh <mreizvykh@gmail.com>
2023-01-28 16:20:25 +0200

    provide categories tree instead of flat array

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Category } from './category.entity';
 import { Repository } from 'typeorm';
+import { Category } from './category.entity';
+import { CategoryTreeNode } from './categories.types';
 
 @Injectable()
 export class CategoriesService {
@@ -10,7 +11,38 @@ export class CategoriesService {
     private categoriesRepository: Repository<Category>,
   ) {}
 
-  public async getCategoriesTree(): Promise<any[]> {
-    return this.categoriesRepository.find();
+  private insertNodeInTree(node: CategoryTreeNode, tree: CategoryTreeNode[]) {
+    if (tree.length === 0) return;
+
+    for (let i = 0; i < tree.length; i += 1) {
+      if (tree[i].id === node.parentId) {
+        tree[i].children.push(node);
+        return;
+      }
+
+      this.insertNodeInTree(node, tree[i].children);
+    }
+  }
+
+  private listToTree(categories: Category[]): CategoryTreeNode[] {
+    const categoryNodes: CategoryTreeNode[] = categories.map((category) => ({
+      ...category,
+      children: [],
+    }));
+
+    const topCategories = categoryNodes.filter((c) => c.parentId === null);
+    const subCategories = categoryNodes.filter((c) => c.parentId !== null);
+
+    while (subCategories.length > 0) {
+      this.insertNodeInTree(subCategories[0], topCategories);
+      subCategories.shift();
+    }
+
+    return topCategories;
+  }
+
+  public async getCategoriesTree(): Promise<CategoryTreeNode[]> {
+    const allCategories = await this.categoriesRepository.find();
+    return this.listToTree(allCategories);
   }
 }
